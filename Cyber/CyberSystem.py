6a0634030e8a767b0986e4d5761bc7d98f5c10f2
xy008areshsu <xy008areshsu@gmail.com>
2016-03-17 23:27:31 -0400

    fixed voting issue

--- Cyber/CyberSystem.py
+++ Cyber/CyberSystem.py
@@ -16,6 +16,7 @@ class CyberSystem:
         self.control_inputs = {}
         for name, val in self.control_inputs_candidates.items():
             self.control_inputs[name] = 0
+        self.control_inputs_candidates_iterations = {}
 
 
     def update(self, h, clock, sensor_inputs):
@@ -41,21 +42,21 @@ class CyberSystem:
     def load_tuning(self):
         idle_power = 0.5  # This is absurd, To Be Refactored!!!
         x = np.reshape(self.processors[0].rtos.task_outputs['filter'], (-1, 1))
-        copy = 3
+        copy = 1
         self.processors.sort(key=attrgetter('reliability_model.abs_temperature'))
         for i in range(copy):
             # List of processors need to run the tasks
             for name in self.processors[i].rtos.task_list:
                 if name != 'filter': # filtering task always runs 3 copies, since it is the most important localizations
                     self.processors[i].rtos.task_list[name].power = self.processors[i].rtos.task_profiles[name].power
-                    # self.processors[i].rtos.task_list[name].et = self.processors[i].rtos.task_profiles[name].et
+                    self.processors[i].rtos.task_list[name].et = self.processors[i].rtos.task_profiles[name].et
 
         for i in range(copy,len(self.processors)):
             # List of processor need to get some rest
             for name in self.processors[i].rtos.task_list:
                 if name != 'filter': # filtering task always runs 3 copies, since it is the most important localizations
                     self.processors[i].rtos.task_list[name].power = idle_power
-                    # self.processors[i].rtos.task_list[name].et = 0.001
+                    self.processors[i].rtos.task_list[name].et = 0
                     # Don't change task et for now, since the voting algorithm would give wrong output,
                     # this is fine since the normal condition is to run 3 copies of the task, and now using AdaFT power will be idle,
                     # but the et will be unchanged, thus the schedulibility would not be affected.
@@ -67,13 +68,16 @@ class CyberSystem:
 
     def update_control_inputs(self):
         self.control_inputs_candidates = {}  # reset first
+        self.control_inputs_candidates_iterations = {}  # reset first
         for processor in self.processors:
             for control_name, control_val in processor.rtos.task_outputs.items():
                 if control_name != 'filter':
                     if control_name in self.control_inputs_candidates:
                         self.control_inputs_candidates[control_name].append(control_val)
+                        self.control_inputs_candidates_iterations[control_name].append(processor.rtos.task_iterations[control_name])
                     else:
                         self.control_inputs_candidates[control_name] = [control_val]
+                        self.control_inputs_candidates_iterations[control_name] = [processor.rtos.task_iterations[control_name]]
 
     @staticmethod
     def triple_vote(val_list, epsilon):
@@ -139,12 +143,14 @@ class CyberSystem:
         #                 self.actuator_commands[command]['output'] = self.actuator_commands[command]['val_list'][p]
 
         for control_name, control_val_list in self.control_inputs_candidates.items():
-            if len(control_val_list) == 3:
-                self.control_inputs[control_name] = self.triple_vote(control_val_list, 1e-6)
-            elif len(control_val_list) == 2:
-                self.control_inputs[control_name] = self.double_vote(control_val_list, 1e-6)
-            else:
-                self.control_inputs[control_name] = self.control_inputs_candidates[control_name][0]
+            # only when iterations numbers are equal to each other, it means all copies of a task have finished for this iteration, then we use voting
+            if all(x==self.control_inputs_candidates_iterations[control_name][0] for x in self.control_inputs_candidates_iterations[control_name]):
+                if len(control_val_list) == 3:
+                    self.control_inputs[control_name] = self.triple_vote(control_val_list, 1e-6)
+                elif len(control_val_list) == 2:
+                    self.control_inputs[control_name] = self.double_vote(control_val_list, 1e-6)
+                else:
+                    self.control_inputs[control_name] = self.control_inputs_candidates[control_name][0]
 
 
 
